ea2fcc2e210470e999795df84c23f3b14c9a7084
Your Name <andangsaefullah4@gmail.com>
2020-10-24 14:24:33 +0700

    Add project files.

---
+++ RentalKendaraan_032/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RentalKendaraan_032.MvcApplication" Language="C#" %>
---
+++ RentalKendaraan_032/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentalKendaraan_032.Startup))]
+namespace RentalKendaraan_032
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
